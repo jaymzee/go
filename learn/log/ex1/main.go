4bff500d085664c9ea3e059007937fdd2104ceef
jaymzee <jaymzee@users.noreply.github.com>
2020-10-06 05:05:10 -0400

    learn/log

---
+++ learn/log/ex1/main.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"log"
+	"os"
+)
+
+const logFlags = log.Ldate | log.Ltime | log.Lmicroseconds | log.Lshortfile
+
+func sum(x int, y int) int {
+	result := x + y
+	log.Printf("sum(%d, %d) %d\n", x, y, result)
+	return result
+}
+
+func main() {
+	log.SetFlags(logFlags)
+	log.SetOutput(os.Stdout)
+	log.SetPrefix("log: ")
+	z := 0
+	z = sum(z, 5)
+	z = sum(z, 8)
+	log.Println("z =", z)
+	log.Println("goodbye")
+}
