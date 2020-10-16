10f6d9d4a123b5d00e9390ab83d821b35dc3c74b
jaymzee <jaymzee@users.noreply.github.com>
2020-10-15 20:39:59 -0400

    fft - ReverseBits

--- fft/main.go
+++ fft/main.go
@@ -11,6 +11,19 @@ func Twiddle(N int) complex128 {
 	return cmplx.Exp(complex(0, angle))
 }
 
+func ReverseBits(x uint, w uint) uint {
+	x = (x & 0xaaaaaaaa) >> 1 | (x & 0x55555555) << 1
+	x = (x & 0xcccccccc) >> 2 | (x & 0x33333333) << 2
+	x = (x & 0xf0f0f0f0) >> 4 | (x & 0x0f0f0f0f) << 4
+	x = (x & 0xff00ff00) >> 8 | (x & 0x00ff00ff) << 8
+	x = x >> 16 | x << 16
+
+	return x >> (32 - w)
+}
+
+
 func main() {
-	fmt.Println(Twiddle(8))
+	for i := uint(0); i < 16; i++ {
+		fmt.Printf("%02x %02x\n", i, ReverseBits(i, 4))
+	}
 }
