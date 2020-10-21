2744678976cebae534d3808740ac465caa3b9fab
jaymzee <jaymzee@users.noreply.github.com>
2020-10-21 11:33:21 -0400

    examples/asm/vector - tests

--- examples/asm/vector/funcs_test.go
+++ examples/asm/vector/funcs_test.go
@@ -8,3 +8,26 @@ func TestAddInt(t *testing.T) {
 		t.Errorf("AddInt(2, 3) = %d; want 6", got)
 	}
 }
+
+func TestMultInt_small(t *testing.T) {
+	lo, _ := MultInt(2, 3)
+	if lo != 6 {
+		t.Errorf("MultInt(2, 3) = %d; want 6", lo)
+	}
+}
+
+func TestMultInt_big(t *testing.T) {
+	a, b := 0x20003, 0x3002
+	lo, hi := MultInt(a, b)
+	if hi != 0 || lo != 0x60049006 {
+		t.Errorf("MultInt(%#x, %#x) = %#x-%x; want 0x6-0", a, b, hi, lo)
+	}
+}
+
+func TestMultFloat32(t *testing.T) {
+	var a, b float32 = 2.0, 3.0
+	got := MultFloat32(a, b)
+	if got != 6.0 {
+		t.Errorf("MultFloat32(%v, %v) = %v; want 6", a, b, got)
+	}
+}
--- examples/asm/vector/main.go
+++ examples/asm/vector/main.go
@@ -3,13 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	var a, b int
+	var lo, hi int
 	fmt.Println("2 + 3 =", AddInt(2, 3))
 
-	a, b = MultInt(2, 3)
-	fmt.Println("2 * 3 =", a)
-	a, b = MultInt(0x3, 0x4)
-	fmt.Printf("0x3 * 0x4 = 0x%016x%016x\n", b, a)
+	lo, hi = MultInt(2, 3)
+	fmt.Println("2 * 3 =", lo)
+	lo, hi = MultInt(0x3, 0x4)
+	fmt.Printf("0x3 * 0x4 = 0x%016x%016x\n", hi, lo)
 	fmt.Printf("%v\n", MultFloat32(3.14, 2.7))
 	fmt.Printf("%v\n", MultFloat64(3.14, 2.7))
 }
