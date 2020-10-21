986d5f14545e79d5e298be314841575851799600
jaymzee <jaymzee@users.noreply.github.com>
2020-10-21 03:20:27 -0400

    examples/struct - &Point2

--- examples/struct/main.go
+++ examples/struct/main.go
@@ -9,7 +9,9 @@ type T struct {
 }
 
 func main() {
-	s := T{a:3, b:5, c:3.14, d:"joe"}
+	s := T{a: 3, b: 5, c: 3.14, d: "joe"}
 	s.b = 42
+	t := &T{a: 5, b: 6, c: 7, d: "george"}
 	fmt.Printf("%#v\n", s)
+	fmt.Printf("%#v\n", t)
 }
