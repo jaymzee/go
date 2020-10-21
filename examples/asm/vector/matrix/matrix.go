00474368244e0d1e48246f0190e61695c2f5d3e4
jaymzee <jaymzee@users.noreply.github.com>
2020-10-21 14:59:52 -0400

    examples/asm/vector/matrix - comments

--- examples/asm/vector/matrix/matrix.go
+++ examples/asm/vector/matrix/matrix.go
@@ -1,3 +1,7 @@
 package matrix
 
+// Matrix4x4v computes the Matrix vector product
+// rows matrix
+// x source vector
+// y result vector
 func Matrix4x4v(rows *[4][4]float32, x *[4]float32, y *[4]float32)
