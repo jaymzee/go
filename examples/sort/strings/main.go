66d767f510e23d0d2cae1b6145b3c0895d308c90
jaymzee <jaymzee@users.noreply.github.com>
2020-10-21 04:15:53 -0400

    examples/sort - added SearchString

--- examples/sort/strings/main.go
+++ examples/sort/strings/main.go
@@ -5,8 +5,21 @@ import (
 	"sort"
 )
 
+func fileString(xs []string, x string) []string {
+	i := sort.SearchStrings(xs, x)
+	ys := append(xs, "")
+	copy(ys[i+1:], xs[i:])
+	ys[i] = x
+	return ys
+}
+
 func main() {
-	x := []string{"pear", "apple", "banana"}
-	sort.Strings(x)
-	fmt.Println(x)
+	fruit := []string{"pear", "apple", "banana"}
+	fmt.Println(fruit, sort.StringsAreSorted(fruit))
+	sort.Strings(fruit)
+	fmt.Println(fruit, sort.StringsAreSorted(fruit))
+	fruit = fileString(fruit, "peasoup")
+	fruit = fileString(fruit, "bananrama")
+	fruit = fileString(fruit, "aardvark")
+	fmt.Println(fruit, sort.StringsAreSorted(fruit))
 }
