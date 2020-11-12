85db89b5d8690908edac3d8dfc1b30c2d771cbdf
jaymzee <jaymzee@users.noreply.github.com>
2020-11-12 07:27:14 -0500

    examples/asm/vec/matrix: comments

--- examples/asm/vec/matrix/matrix.go
+++ examples/asm/vec/matrix/matrix.go
@@ -1,7 +1,7 @@
 package matrix
 
-// Matrix4x4v computes the Matrix vector product
-// rows matrix
+// Matrix4x4v computes the product of a matrix and a vector
+// rows 4x4 matrix
 // x source vector
 // y result vector
 func Matrix4x4v(rows *[4][4]float32, x *[4]float32, y *[4]float32)
--- examples/asm/vec/matrix/matrix_amd64.s
+++ examples/asm/vec/matrix/matrix_amd64.s
@@ -1,20 +1,24 @@
 #include "textflag.h"
 
+// Matrix4x4v computes the product of a Matrix and a vector
+// rows *[4][4]float32
+// x *[4]float32
+// y *[4]float32
 TEXT ·Matrix4x4v(SB),NOSPLIT,$0
 	MOVQ	rows+0(FP), DI
 	MOVQ	x+8(FP), SI
 	MOVQ	y+16(FP), BX
 	MOVAPS	0(SI), X3
 	MOVAPS	0(DI), X0
-	MULPS	X3, X0
+	MULPS	X3, X0		// X0 ← rows[0] * x
 	MOVAPS	16(DI), X1
-	MULPS	X3, X1
-	HADDPS	X1, X0
+	MULPS	X3, X1		// X1 ← rows[1] * x
+	HADDPS	X1, X0		// X0 ← partial sum of products first two rows
 	MOVAPS	32(DI), X1
-	MULPS	X3, X1
+	MULPS	X3, X1		// X1 ← rows[2] * x
 	MOVAPS	48(DI), X2
-	MULPS	X3, X2
-	HADDPS	X2, X1
+	MULPS	X3, X2		// X2 ← rows[3] * x
+	HADDPS	X2, X1		// X1 ← partial sum of products next two rows
 	HADDPS	X1, X0
-	MOVAPS	X0, 0(BX)
+	MOVAPS	X0, 0(BX)	// y ← total sum of products
 	RET
