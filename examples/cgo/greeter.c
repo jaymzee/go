c4311be70f369323c0dfc7edc1cb93c10d25380c
jaymzee <jaymzee@users.noreply.github.com>
2020-11-14 09:16:47 -0500

    examples/cgo: import constants from C

--- examples/cgo/greeter.c
+++ examples/cgo/greeter.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 #include "greeter.h"
 
+int OtherNumber = 88;
+
 int greet(const char *name, int year, char *out) {
     int n = sprintf(out, "Greetings, %s from %d! We come in peace :)",
                     name, year);
