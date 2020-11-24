78ab7f668ff9e3769030d0dbe7091f03d62144db
jaymzee <jaymzee@users.noreply.github.com>
2020-11-24 12:46:18 -0500

    examples/asm/vec/funcs: _ more readable constants

--- examples/asm/vec/funcs/funcs_amd64_test.go
+++ examples/asm/vec/funcs/funcs_amd64_test.go
@@ -3,9 +3,9 @@ package main
 import "testing"
 
 func TestMultInt_bigger(t *testing.T) {
-	a, b := 0x200000005, 0x300000007
+	a, b := 0x2_00000005, 0x3_00000007
 	lo, hi := MultInt(a, b)
-	if hi != 6 || lo != 0x1d00000023 {
+	if hi != 6 || lo != 0x1d_00000023 {
 		t.Errorf("MultInt(%#x, %#x) = %#x-%x; want 0x6-0", a, b, hi, lo)
 	}
 }
