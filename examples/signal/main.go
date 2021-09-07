87fdf4164f6391aa0cb8d48b89d63c293a4453d1
jaymzee <jaymzee@users.noreply.github.com>
2021-09-07 11:12:18 -0400

    examples/signal os.Signal should be buffered

--- examples/signal/main.go
+++ examples/signal/main.go
@@ -20,7 +20,7 @@ func main() {
 }
 
 func setupCloseHandler(resource int) {
-	c := make(chan os.Signal)
+	c := make(chan os.Signal, 1)
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
