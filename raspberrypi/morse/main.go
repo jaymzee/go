bf6334326512e50eec0d0c5c458264e200934a4d
jaymzee <jaymzee@users.noreply.github.com>
2021-09-07 10:16:58 -0400

    updated morse to use updated gpio0

--- raspberrypi/morse/main.go
+++ raspberrypi/morse/main.go
@@ -8,50 +8,56 @@ import (
 	"log"
 	"os"
 	"os/signal"
-	"strconv"
 	"strings"
 )
-
-var pinNumber int
-
-func init() {
-	flag.Usage = func() {
-		fmt.Fprintf(os.Stderr, "Usage: %s pin message\n", os.Args[0])
-	}
-}
 
 func main() {
 	// configure logging
 	log.SetFlags(log.Ltime | log.Lmicroseconds)
 
-	// parse flags and check usage
+	// flags
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options] message\n", os.Args[0])
+		fmt.Fprintf(os.Stderr, "options:\n")
+		flag.PrintDefaults()
+	}
+	gflag := flag.Int("g", -1, "gpio pin number")
+	oflag := flag.Int("o", 1, "onboard led number")
 	flag.Parse()
 	args := flag.Args()
-	if len(args) < 2 {
+
+	if len(args) < 1 {
 		flag.Usage()
 		os.Exit(1)
 	}
+
+	message := strings.ToUpper(strings.Join(args, " "))
+
+	var led *gpio0.LED
 	var err error
-	pinNumber, err = strconv.Atoi(args[0])
-	if err != nil {
-		flag.Usage()
-		os.Exit(1)
-	}
-	message := strings.ToUpper(strings.Join(args[1:], " "))
 
-	// setup the remaining things
-	led, err := gpio0.OpenLED(pinNumber)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
+	if *gflag >= 0 {
+		led, err = gpio0.OpenGPIO(*gflag, false)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		fmt.Printf("Sending morse code to gpio pin %d\n", *gflag)
+	} else {
+		led, err = gpio0.OpenLocal(*oflag)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+		fmt.Printf("Sending morse code to onboard led %d\n", *oflag)
 	}
+
 	setupCtrlCHandler(led)
-
-	// loop sending message forever
-	fmt.Printf("Sending morse code on gpio pin %d\n", pinNumber)
 	for {
-		// morse.Send(led, messgae)
-		io.WriteString(led, message)
+		_, err = io.WriteString(led, message)
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
@@ -62,7 +68,11 @@ func setupCtrlCHandler(led *gpio0.LED) {
 
 	go func() {
 		<-c
-		led.Off()
+		if led.Local() && led.Pin() == 1 {
+			led.On() // this is the power good (input) led
+		} else {
+			led.Off() // everything else should be turned off
+		}
 		os.Exit(0)
 	}()
 }
