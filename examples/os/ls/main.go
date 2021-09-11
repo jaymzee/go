681bd2da6912bfcb58829341a6f01184e3642c89
jaymzee <jaymzee@users.noreply.github.com>
2021-09-11 14:47:36 -0400

    examples/os/ls refactored

--- examples/os/ls/main.go
+++ examples/os/ls/main.go
@@ -4,6 +4,7 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"strconv"
 	"strings"
@@ -11,47 +12,87 @@ import (
 )
 
 func main() {
-	dirname := "."
+	args := []string{"."}
 	if len(os.Args) > 1 {
-		dirname = os.Args[1]
+		args = os.Args[1:]
 	}
+	for _, arg := range args {
+		ls(arg)
+	}
+}
 
-	// read the directory
-	dir, err := os.ReadDir(dirname)
+func ls(p string) {
+	// arg is directory or file?
+	info, err := os.Stat(p)
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
+	if info.IsDir() {
+		// read the directory
+		dir, err := os.ReadDir(p)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
 
-	// find the largest size and get it's width in characters
+		// print the directory
+		err = PrintDirEntries(os.Stdout, dir)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	} else {
+		// it's just a file so print the info for that
+		err = PrintFileInfo(os.Stdout, info, 1)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, err)
+			os.Exit(1)
+		}
+	}
+}
+
+func PrintDirEntries(w io.Writer, entries []os.DirEntry) error {
+	// find the largest size and get the length of it's string repr
 	var maxSize int64 = 0
-	for _, entry := range dir {
+	for _, entry := range entries {
 		info, err := entry.Info()
 		if err != nil {
-			panic(err)
+			return err
 		}
 		if info.Size() > maxSize {
 			maxSize = info.Size()
 		}
 	}
-	sizeWidth := len(strconv.FormatInt(maxSize, 10))
+	sizewidth := len(strconv.FormatInt(maxSize, 10))
 
-	// print the directory
-	for _, entry := range dir {
+	// write the directory to the writer
+	for _, entry := range entries {
 		info, err := entry.Info()
 		if err != nil {
-			panic(err)
+			return err
 		}
-		fmt.Printf("%s", info.Mode())
-		if stat, ok := info.Sys().(*syscall.Stat_t); ok {
-			fmt.Printf(" %d %d %d", stat.Nlink, stat.Uid, stat.Gid)
-		}
-		mtime := info.ModTime().Format("Jan 02 15:04")
-		size := padLeft(strconv.FormatInt(info.Size(), 10), ' ', sizeWidth)
-		fmt.Printf(" %s %12s %s\n", size, mtime, entry.Name())
+		PrintFileInfo(w, info, sizewidth)
 	}
+
+	return nil
+}
+
+func PrintFileInfo(w io.Writer, info os.FileInfo, sizwidth int) error {
+	fmt.Fprintf(w, "%s", info.Mode())
+	if stat, ok := info.Sys().(*syscall.Stat_t); ok {
+		fmt.Fprintf(w, " %d %d %d", stat.Nlink, stat.Uid, stat.Gid)
+	}
+	mtime := info.ModTime().Format("Jan 02 15:04")
+	size := padLeft(strconv.FormatInt(info.Size(), 10), ' ', sizwidth)
+	fmt.Fprintf(w, " %s %12s %s\n", size, mtime, info.Name())
+	return nil
 }
 
 func padLeft(str string, pad rune, length int) string {
-	return strings.Repeat(string(pad), length-len(str)) + str
+	if length > len(str) {
+		count := length - len(str)
+		return strings.Repeat(string(pad), count) + str
+	}
+	return str
 }
