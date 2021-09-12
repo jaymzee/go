fa6d34b1c490b9466c130dd279f5520e19309d49
jaymzee <jaymzee@users.noreply.github.com>
2021-09-12 03:04:33 -0400

    examples/http/s/gin

--- examples/http/s/service/main.go
+++ examples/http/s/service/main.go
@@ -7,14 +7,14 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/hello", helloHandler)
+	http.HandleFunc("/hello/", helloHandler)
 	http.HandleFunc("/form", formHandler)
 	fmt.Println("listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", http.DefaultServeMux))
 }
 
-func helloHandler(w http.ResponseWriter, _ *http.Request) {
-	fmt.Fprint(w, "hello world!")
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "hello world! %s", r.URL)
 }
 
 func formHandler(w http.ResponseWriter, r *http.Request) {
